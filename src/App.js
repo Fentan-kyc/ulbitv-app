d5d1dae5be99761a94a28dcc35471f4e454d033f
Fentan-kyc <di-ma02@mail.ru>
2022-07-18 17:09:41 +0300

    buttons without useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
+import React from "react";
 
 function App() {
+
+  let i = 0;
+
+  function Plus() {
+    i += 1
+    console.log(i)
+  }
+
+  function Minus() {
+    i -= 1
+    console.log(i)
+  }
+
   return (
     <div className="App">
+      <h1>{i}</h1>
+      <button onClick={In}>Plus</button>
+      <button onClick={() => { i -= 1 }}>Minus</button>
 
     </div>
   );
