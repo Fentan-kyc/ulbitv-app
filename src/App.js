b5d03a689addfed3942abbb0d3bdc47826791cd1
Fentan-kyc <di-ma02@mail.ru>
2022-07-19 13:13:57 +0300

    working with create/remove callbacks

--- src/App.js
+++ src/App.js
@@ -17,10 +17,14 @@ function App() {
     setPosts([...posts, newPost])
   }
 
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList title='List' posts={posts} />
+      <PostList remove={removePost} title='List' posts={posts} />
     </div>
   );
 }
--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PostItem from './PostItem'
 
-const PostList = (props) => {
+const PostList = ({ posts, title, remove }) => {
     return (
         <div>
-            <h2 style={{ textAlign: 'center' }}>{props.title}</h2>
-            {props.posts.map((post, index) =>
-                <PostItem number={index + 1} post={post} key={post.id} />)}
+            <h2 style={{ textAlign: 'center' }}>{title}</h2>
+            {posts.map((post, index) =>
+                <PostItem remove={remove} number={index + 1} post={post} key={post.id} />)}
         </div>
     )
 }
