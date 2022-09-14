8a9747cf66c34c3efd6cc353271653d99bd76495
Fentan-kyc <di-ma02@mail.ru>
2022-09-14 19:57:21 +0300

    Post comments

--- src/API/PostService.jsx
+++ src/API/PostService.jsx
@@ -12,8 +12,14 @@ export default class PostService {
     }
 
     static async getById(id){
-        console.log(id)
         const response = await axios.get('https://jsonplaceholder.typicode.com/posts/' + id);
         return response;
 }
+
+static async getCommentsByPostId(id){
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+    return response;
+}
+
+
 }
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -6,14 +6,22 @@ import { useFetching } from '../hooks/useFething';
 
 const PostPage = () => {
     const params = useParams();
-    const [post,setPost] = useState(0)
+    const [post,setPost] = useState({});
+    const [comments,setComments] = useState([])
     const [fetchingPostById, isLoading, error] = useFetching( async (id) => {
         const response = await PostService.getById(id);
         setPost(response.data);
     });
 
+    const [fetchingComments, isComLoading, comError] = useFetching( async (id) => {
+      const response = await PostService.getCommentsByPostId(id);
+      setComments(response.data);
+      console.log(comments)
+  });
+
     useEffect(() => {
         fetchingPostById(params.id)
+        fetchingComments(params.id)
     }, []);
 
   return (
@@ -24,6 +32,14 @@ const PostPage = () => {
         ? <Loader/>
         : <div>{post.id}. {post.title}</div>
     }
+
+    <h1>Комментарии</h1>
+
+    {isComLoading
+        ? <Loader/>
+        : <div>{comments.map( (comm) => <div key={comm.id} style={{marginTop: 15}}><h5>{comm.email}</h5><div>{comm.body}</div> </div>)}</div>
+    }
+    
     </div>
   )
 }
